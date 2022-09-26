6623ede461dc7d6b5addb8f3ec64dc9ce1bfea86
Mila <milaprivalova1@gmail.com>
2022-09-26 16:02:41 +0300

    task2

--- main.py
+++ main.py
@@ -6,8 +6,21 @@
 # - 6 -> да
 # - 7 -> да
 # - 1 -> нет
-x = int(input("Введите число от 1 до 7"))
-if x < 6:
-    print("нет")
-elif x == 6 or x == 7:
-    print("да")
+# x = int(input("Введите число от 1 до 7"))
+# if x < 6:
+#   print("нет")
+# elif x == 6 or x == 7:
+#   print("да")
+
+ # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
+# для всех значений предикат.
+
+print('¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z')
+print()
+print('x, y, z')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(x, y, z)
+            print((int(not (x or y or z))) == (not x and not y and not z))
+
